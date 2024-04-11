78559cd0ec37f95fa7f1e810b82549a3bbe3ebaa
Kilian Abel Anibal Flores <kiliananibal@gmail.com>
2024-04-10 22:01:54 -0700

    10/04/2024 22:02

--- script.js
+++ script.js
@@ -13,5 +13,19 @@ console.log("His age is "+age+" and one of his favorites hobbies is : "+hobbies[
 //ACTIVITY 3
 let ToDoList = ["eat my dinner","wash my clothes","do exercises"];
 
+let DeskLamp = {
+    color:'green',
+    brand: "XYZ",
+    model: "SmartDesk Lamp",
+    power: 15, // in watts
+    isAdjustable: true,
+    hasUSBPort: false,
+    dimensions: {
+        width: 20, // in centimeters
+        height: 45, // in centimeters
+        depth: 15 // in centimeters
+    }
+}
 
-
+console.log("The color of the lamp is "+DeskLamp.color)
+console.log("His model is "+DeskLamp.brand+" and his power in watts are "+DeskLamp.power)
