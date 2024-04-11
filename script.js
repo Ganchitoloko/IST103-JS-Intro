24754896c94440d8229358bbdbca3b37e252643b
Kilian Abel Anibal Flores <kiliananibal@gmail.com>
2024-04-10 21:33:14 -0700

    10/04/2024 21:33

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+//ACTIVITY 1
 alert("hello world");
+
+//ACTIVITY 2
+let PersonName = "Pedro";
+let age = 25;
+let isStudent = true;
+let hobbies = ["reading","paiting","hiking"];
+
+console.log("The name of this person is "+ PersonName);
+console.log("His age is "+age+" and one of his favorites hobbies is : "+hobbies[0])
+
+
+
