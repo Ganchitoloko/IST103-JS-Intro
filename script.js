f83dcbe811bfc543a79c47004654a311db09cae6
Kilian Abel Anibal Flores <kiliananibal@gmail.com>
2024-04-10 21:47:05 -0700

    10/04/2024 21:47

--- script.js
+++ script.js
@@ -10,5 +10,8 @@ let hobbies = ["reading","paiting","hiking"];
 console.log("The name of this person is "+ PersonName);
 console.log("His age is "+age+" and one of his favorites hobbies is : "+hobbies[0])
 
+//ACTIVITY 3
+let ToDoList = ["eat my dinner","wash my clothes","do exercises"];
+
 
 
